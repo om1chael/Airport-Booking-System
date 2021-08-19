fde28dd97aa6f1d8f87b15fe4de125f33cc5c656
ViMitre <vmitre@spartaglobal.com>
2021-08-19 15:40:52 +0000

    Flask and html templates continued

--- airport_booking_system/flask_app/app.py
+++ airport_booking_system/flask_app/app.py
@@ -1,29 +1,37 @@
 from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# @app.route('/create_flight')
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/flight')
-# def index():
-#     return render_template('index.html')
+    flight_list = ['flight1', 'flight2']
+    if request.method == 'POST':
+        FlightList = request.form['dropdown']
+    return render_template('index.html', flight_list=flight_list)
 
 
+@app.route('/create_flight', methods=["GET", "POST"])
+def create_flight():
+    if request.method == 'POST':
 
 
-@app.route('/data', methods=["GET", "POST"])
-def data():
-    if request.method == "GET":
-        return '<h1>Data works</h1>'
-    elif request.method == "POST":
-        return 'POST'
+    return render_template('create_flight.html')
+
+@app.route('/flight_trip')
+def flight_trip():
+    return render_template('flight_trip.html')
+
+
+
+
+# @app.route('/data', methods=["GET", "POST"])
+# def data():
+#     if request.method == "GET":
+#         return '<h1>Data works</h1>'
+#     elif request.method == "POST":
+#         return 'POST'
 
 
 if __name__ == '__main__':
