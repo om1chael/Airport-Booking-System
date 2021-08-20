18b19a533079486f97f1b41455c5e46ef3545f3f
ViMitre <vmitre@spartaglobal.com>
2021-08-20 10:29:38 +0000

    Friday 4

--- airport_booking_system/airport_booking/json_planes.py
+++ airport_booking_system/airport_booking/json_planes.py
@@ -1,4 +1,5 @@
 import json
+from definitions import ROOT_DIR, json_path
 from airport_booking_system.airport_booking.plane import Plane
 
 
@@ -7,10 +8,10 @@ def create_json_planes_file(plane_id, plane_cap):
         "id": plane_id,
         "max capacity": plane_cap
             }
-    with open("planes.json", "r") as file:
+    with open(json_path + "planes.json", "r") as file:
         data = json.load(file)
     data.append(planes_dict)
-    with open("planes.json", "w") as file:
+    with open(json_path + "planes.json", "w") as file:
         json.dump(data, file)
     return
 
