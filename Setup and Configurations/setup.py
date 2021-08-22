a59b5265ef02cff0d98dbea028dd9a5c2b92752a
ViMitre <88156993+ViMitre@users.noreply.github.com>
2021-08-22 17:47:57 +0000

    Delete Setup and Configurations directory

--- Setup and Configurations/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="airport_booking_system",
-    version="0.8.0",
-    description="Airport booking system",
-    authors="Akunma Ndujiuba, Michael Tefera, Sacha Dorf, Viktor Mitre",
-    packages=["airport_booking_system", "airport_booking_system.flask_app"]
-)
