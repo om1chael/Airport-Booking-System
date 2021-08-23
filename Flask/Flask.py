bef7acbbcd47cc92e0b31afeb36a6c0befbbe609
omichael <omichaeltmichael@live.co.uk>
2021-08-23 02:42:39 +0100

    show and add passengers

--- Flask/Flask.py
+++ Flask/Flask.py
@@ -3,57 +3,58 @@ from flask import Flask, render_template, redirect, url_for, request
 import requests
 import re
 import os
-from definitions import ROOT_DIR, json_path
+from Project.Setup_and_Configurations.definitions import ROOT_DIR, json_path
 import json
-
+from Project.airport_booking_system.airport_booking import passenger
+import pathlib
 app = Flask(__name__)
-filepath = os.path.join(os.path.dirname(__file__) + "/current_flights.json")
-
 
 # current_flights.json
-def read_file():
-    with open(json_path + 'flight_trips.json', ) as flights:
+def read_file(file_name):
+    with open( file_name, ) as flights:
         data = flights.read()
-        print(type(data))
     return json.loads(data)
+
 
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    json_file = read_file()
-    print("2", type(json_file))
     if request.method == 'GET':
-        user = read_file()
-        print(3, user)
+
+        user = read_file("flight_trips.json")
         return render_template("index.html",
                                json_file=user)
     else:
         user = re.search("[^=][A-Z\d]+", str(request.get_data('fly'))).group()
-        print(user, "000000", json_file[str(user)])
-        # main=json_file[str(user)][0]
+       # print(user, "000000", json_file[str(user)])
 
         return redirect(url_for('success', id=user))
 
 
 @app.route('/success/<id>', methods=['POST', 'GET'])
 def success(id):
+    pass_file = read_file('passengers.json')
+    user = read_file('flight_trips.json')
+
     if request.method == 'GET':
-        user = read_file()
         return render_template("third_page.html",
                                plane_id=id,
-                               data=user[id]
+                               data=user[id],
+                               pass_info=pass_file[id][0])
+    else:
+
+        pass_id = request.form["passport_ID"]
+        name = request.form["Name"]
+        creat_pass=passenger.Passenger(id,pass_id ,name)
+        creat_pass.create_json_passenger_file()
+        return render_template("third_page.html",
+                               plane_id=id,
+                               data=user[id],
+                               pass_info=pass_file[id][0]
+
                                )
 
 
-# response_code = requests.get("http://127.0.0.1:5000/")
-# print(response_code)
-
-@app.route("/api", methods=["GET", "POST"])
-def data():
-    if request.method == "GET":
-        return '<h1 style ="post:green"> The test works </h1>'
-    elif request.method == "POST":
-        return 'POST'
 
 
 if __name__ == "__main__":
