4ead51f9e7140c0ae2b7dbb07e7bb492778bd8c5
omichael <omichaeltmichael@live.co.uk>
2021-08-24 10:46:18 +0100

    week 3 work

--- airport_booking_system/airport_booking/flight_trip.py
+++ airport_booking_system/airport_booking/flight_trip.py
@@ -1,5 +1,6 @@
 import json
-from Project.config.definitions import json_path
+from config.definitions import json_path
+from airport_booking_system.airport_booking.plane import Plane
 
 # Define Flight Trip class
 class FlightTrip:
@@ -17,7 +18,7 @@ class FlightTrip:
     def create_flight_trip(self):
         flight_dict = {self.flight_id: [{
             "Destination": self.destination,
-            "Date:Time": self.datetime,
+            "Date-Time": self.datetime,
             "Duration": self.duration,
             "Price": self.price,
             "Plane_ID": self.plane_id,
@@ -30,12 +31,14 @@ class FlightTrip:
             json.dump(data, file)
 
     # Assigns a new plane if the max capacity of the new plane is greater than the number of passengers on the flight
-    def set_plane(self, new_plane):
-        if new_plane.max_capacity > len(self.people):
-            self.plane_id = new_plane.id
-            self.plane_max = new_plane.max_capacity
-        else:
-            return "Plane capacity is too small, you cannot choose this plane."
+    def set_plane(self, flight_id, new_plane):
+        # self.plane_id = new_plane.id
+        # self.plane_max = new_plane.max_capacity
+        with open(json_path + "flight_trips.json", "r+") as file:
+            data = json.load(file)
+            data[flight_id][0]["Plane_ID"] = new_plane
+            file.seek(0)
+            json.dump(data, file)
 
     # Returns the available capacity on the plane
     def return_capacity(self):
@@ -57,5 +60,3 @@ class FlightTrip:
     # Generates report of the list of passengers on the flight
     def generate_report(self):
         return self.people
-
-
